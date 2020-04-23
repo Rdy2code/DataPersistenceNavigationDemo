b38bf4362a0e477e425f5e0df0aaa0f8263f23e5
Louis Wollenberger <lvwmailbox@yahoo.com>
2020-04-23 11:13:23 -0700

    Add sharedpreferences class

---
+++ app/src/main/java/com/example/androiddata/utilities/PrefsHelper.kt
@@ -0,0 +1,28 @@
+package com.example.androiddata.utilities
+
+import android.content.Context
+import android.content.SharedPreferences
+
+const val ITEM_TYPE_KEY = "item_type_key"
+
+class PrefsHelper {
+
+    companion object{
+        private fun preferences (context: Context) : SharedPreferences =
+            context.getSharedPreferences("default", 0)
+
+        fun setItemType (context: Context, type: String) {
+
+            //Get instantiate the preferences object/file, in edit mode add
+            //the key value pair
+            preferences(context)
+                .edit()
+                .putString(ITEM_TYPE_KEY, type)
+                .apply()
+        }
+
+        fun getItemType (context: Context): String {
+            return preferences(context).getString(ITEM_TYPE_KEY, "list")!!
+        }
+    }
+}
